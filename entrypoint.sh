43468163ca2fa5c526ebb33851a75e088761de0f
CHECKago <mfalcon@mail.ru>
2023-12-19 21:32:24 +0300

    finish

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
 
-gunicorn invest.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
+gunicorn SteauaNordului.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
